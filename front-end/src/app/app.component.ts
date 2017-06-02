7afb831d49cc688466ce5d9f2a20f5105914dd73
javasabr <javasabr@gmail.com>
2017-06-02 11:39:10 +0300

    updated front-end

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,18 +1,15 @@
 import {Component, NgModule} from "@angular/core";
+import {AlertModule} from "ngx-bootstrap";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
-
-  logout() {}
-}
-
 @NgModule({
+  imports: [AlertModule.forRoot()]
 })
-export class PizzaPartyAppModule {
+export class AppComponent {
+  logout() {
+  }
 }
-
--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -26,6 +26,7 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+
   }
 
   /**
