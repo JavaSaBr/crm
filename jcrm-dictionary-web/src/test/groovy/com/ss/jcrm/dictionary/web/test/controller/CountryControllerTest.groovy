b3bce6c51671f41938973940709ba6c7a9aa4d66
JavaSaBr <javasabr@gmail.com>
2019-03-26 09:57:43 +0300

    format

--- jcrm-dictionary-web/src/test/groovy/com/ss/jcrm/dictionary/web/test/controller/CountryControllerTest.groovy
+++ jcrm-dictionary-web/src/test/groovy/com/ss/jcrm/dictionary/web/test/controller/CountryControllerTest.groovy
@@ -51,33 +51,32 @@ class CountryControllerTest extends DictionarySpecification {
 
         given:
 
-        def country1 = dictionaryTestHelper.newCountry("country1")
-        def country2 = dictionaryTestHelper.newCountry("country2")
-        def country3 = dictionaryTestHelper.newCountry("country3")
+            def country1 = dictionaryTestHelper.newCountry("country1")
+            def country2 = dictionaryTestHelper.newCountry("country2")
+            def country3 = dictionaryTestHelper.newCountry("country3")
 
-        (countryDictionaryService as Reloadable).reload()
+            (countryDictionaryService as Reloadable).reload()
 
         when:
 
-        def response = client.get()
-            .url("/dictionary/countries")
-            .headerValue("Origin", "http://localhost")
-            .exchange()
+            def response = client.get()
+                .url("/dictionary/countries")
+                .headerValue("Origin", "http://localhost")
+                .exchange()
 
         then:
 
-        response.expectStatus().isOk()
-            .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
-            .expectHeader().exists("Access-Control-Allow-Origin")
-            .expectBody()
-                .jsonPath('$.countries')
-                    .value(hasSize(3))
-                .jsonPath('$.countries[*].name')
-                    .value(containsInAnyOrder(
-                        country1.name,
-                        country2.name,
-                        country3.name
-                )
-        )
+            response.expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectHeader().exists("Access-Control-Allow-Origin")
+                .expectBody()
+                    .jsonPath('$.countries')
+                        .value(hasSize(3))
+                    .jsonPath('$.countries[*].name')
+                        .value(containsInAnyOrder(
+                            country1.name,
+                            country2.name,
+                            country3.name)
+                        )
     }
 }
